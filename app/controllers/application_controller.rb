85ae2fdf8446399f2b769bed77c0dc9de6ba8163
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-04-23 15:34:26 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,24 @@
+require_relative "../models/recipe.rb"
+
 class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get 'recipes/new' do
+    erb :new
+  end
+
+  post '/recipes/new' do
+    @recipe = Recipe.create(name: params[:name], ingredients: params[:ingredients], cook_time: params[:params])
+
+    redirect to("/recipe/#{@recipe.id}")
+  end
 
 end
