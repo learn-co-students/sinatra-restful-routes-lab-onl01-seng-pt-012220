66097538ec7b35309a1026d9d7bb8912f02c1efe
Robert Salaiz <work.salaizrm@gmail.com>
2020-04-24 04:26:03 +0000

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,48 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  # code actions here!
+  # Index should show all recipes (name) with a link to their other values.
+  # EX. Mash potatoes: 2 russet potatoes, 30 minutes (Recipe ID: 1)
+
+  get '/' do
+    redirect '/recipes'
+  end
+
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/:id' do
+    @recipes = Recipe.find_by(params[:id])
+    erb :show
+  end
+
+  post '/recipes/:id' do
+    @recipes = Recipe.create(params)
+    redirect to "/recipes/#{@recipes.id}"
+  end
+
+  get '/recipes/:id/edit' do
+    @recipes = Recipe.find_by(params[:id])
+    erb :edit
+  end
+
+  patch '/articles/:id' do
+    id = params[:id]
+     new_params = {}
+     old = Recipe.find(id)
+     new_params[:name] = params[:name]
+     new_params[:ingredients] = params[:ingredients]
+     new_params[:cook_time] = params[:cook_time]
+     old.update(new_params)
+
+     redirect to "/recipes/#{@recipes.id}"
+  end
+
+  delete '/recipes/:id' do
+    Recipe[0].clear
+    redirect to '/'
+  end
 
 end
