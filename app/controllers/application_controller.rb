e8e491b8382c08d57092dfe844e4f65586d52a4f
vanessuniq <vanessuniq@gmail.com>
2020-05-09 00:40:01 -0400

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,39 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  get '/recipes' do
+    @recipes = Recipe.all
+    erb :index
+  end
+
+  get '/recipes/new' do
+    erb :new
+  end
+
+  post '/recipes' do
+    @recipe = Recipe.create(params)
+    redirect to "/recipes/#{@recipe.id}"
+  end
+
+  get '/recipes/:id' do
+    @recipe1 = Recipe.find(params[:id])
+    erb :show
+  end
+
+  get '/recipes/:id/edit' do
+    @recipe1 = Recipe.find(params[:id])
+    erb :edit
+  end
+
+  patch '/recipes/:id' do
+    @recipe1 = Recipe.find(params[:id])
+    @recipe1.update(params)
+    redirect to "/recipes/#{@recipe1.id}"
+  end
+
+  delete '/recipes/:id' do
+    Recipe.destroy(params[:id])
+    redirect to '/recipes'
+  end
 
 end
